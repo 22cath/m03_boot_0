208d179a2b2b28e784ae8b9f680e58d4f4e9792a
22cath <cecollignon@gmail.com>
2022-02-06 21:13:36 +0100

    map, filter, reduce)

--- FuncionesMapFilterReduce.py
+++ FuncionesMapFilterReduce.py
@@ -1,5 +1,7 @@
 from functools import reduce
 
+# MAP()
+
 def doble(x):
     return x+x
 
@@ -9,7 +11,7 @@ listaDobles = map(lambda x: x*2, lista)
 listaDobles1 = map(doble, lista)
 
 
-# FILTER: sobre los elementos de la lista, solo me vas a devolver los valores x
+# FILTER(): sobre los elementos de la lista, solo me vas a devolver los valores x
 # que cumplan con que el resto de x partido por 2 es igual a zero, y lo aplicas sobre lista. Y si no, nada.
 
 def esPar(x):
@@ -18,7 +20,15 @@ def esPar(x):
 listaPares = filter(lambda x: x % 2 == 0, lista)
 listaPares1 = filter(esPar, lista)
 
+# REDUCE()
 sumatorio = reduce(lambda x, y: x + y, lista)
-
+sumatorioDobles = reduce(lambda x, y: x + y*2, lista)
 suma100 = reduce(lambda x, y: x + y, range(101))
+
+print(list(listaPares))
+print(list(listaPares1))
+
+print(sumatorio)
+print(sumatorioDobles)
+print(suma100)
 
